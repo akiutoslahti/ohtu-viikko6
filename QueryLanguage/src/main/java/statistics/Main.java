6db0e2da6173a66f79615bc585a9d7d9242c69d0
Aki Utoslahti <aki.utoslahti@gmail.com>
2018-11-27 16:09:14 +0200

    querylanguage

---
+++ QueryLanguage/src/main/java/statistics/Main.java
@@ -0,0 +1,25 @@
+package statistics;
+
+import statistics.matcher.Matcher;
+import statistics.matcher.QueryBuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
+
+        QueryBuilder query = new QueryBuilder();
+
+        Matcher m = query.oneOf(
+                query.playsIn("PHI")
+                        .hasAtLeast(10, "goals")
+                        .hasFewerThan(20, "assists").build(),
+                query.playsIn("EDM")
+                        .hasAtLeast(60, "points").build()
+        ).build();
+
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
+    }
+}
